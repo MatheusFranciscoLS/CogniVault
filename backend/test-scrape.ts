9736218ec35cb339fd6b0a451678a82bd7fb81a3
Matheus Francisco <matheusf.ls@hotmail.com>
2026-09-11 12:59:31 -0300

    chore: remove isolated scraper test script

--- backend/test-scrape.ts
+++
@@ -1,8 +0,0 @@
-import { HusqvarnaScraperService } from './src/services/husqvarna-scraper.service';
-
-async function test() {
-    const data = await HusqvarnaScraperService.fetchLiveData('532431650');
-    console.log(JSON.stringify(data, null, 2));
-}
-
-test();
