76fcb36af3445343995058ef1ba3dd12e1f26d5c
Matheus Francisco <matheusf.ls@hotmail.com>
2026-09-16 13:15:27 -0300

    refactor: keep user scoped providers inside routed app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,8 +5,6 @@ import './experience-polish.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider, ThemedToaster } from './components/ThemeProvider'
-import { QuoteCartProvider } from './context/QuoteCartContext'
-import { CounterSessionProvider } from './context/CounterSessionProvider'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,12 +19,8 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="system" storageKey="cognivault-theme">
-        <QuoteCartProvider>
-          <CounterSessionProvider>
-            <App />
-            <ThemedToaster />
-          </CounterSessionProvider>
-        </QuoteCartProvider>
+        <App />
+        <ThemedToaster />
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
