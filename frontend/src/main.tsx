3b456399ccb58462ce9016b4f4a6b65dec5a4120
Matheus Francisco <matheusf.ls@hotmail.com>
2026-09-16 08:58:47 -0300

    feat: apply quote overlay experience lifecycle

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './experience-polish.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider, ThemedToaster } from './components/ThemeProvider'
 import { QuoteCartProvider } from './context/QuoteCartContext'
 import { CounterSessionProvider } from './context/CounterSessionProvider'
 import QuickQuoteCart from './components/QuickQuoteCart'
+import QuoteCartOverlayLifecycle from './components/QuoteCartOverlayLifecycle'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,6 +26,7 @@ createRoot(document.getElementById('root')!).render(
         <QuoteCartProvider>
           <CounterSessionProvider>
             <App />
+            <QuoteCartOverlayLifecycle />
             <QuickQuoteCart />
             <ThemedToaster />
           </CounterSessionProvider>
