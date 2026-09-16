18b5a60ab68aab5d16ee1c3382f45227ed8b587e
Matheus Francisco <matheusf.ls@hotmail.com>
2026-09-15 22:00:04 -0300

    feat: persist active counter session

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider, ThemedToaster } from './components/ThemeProvider'
 import { QuoteCartProvider } from './context/QuoteCartContext'
+import { CounterSessionProvider } from './context/CounterSessionContext'
 import QuickQuoteCart from './components/QuickQuoteCart'
 
 const queryClient = new QueryClient({
@@ -21,9 +22,11 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <ThemeProvider defaultTheme="system" storageKey="cognivault-theme">
         <QuoteCartProvider>
-          <App />
-          <QuickQuoteCart />
-          <ThemedToaster />
+          <CounterSessionProvider>
+            <App />
+            <QuickQuoteCart />
+            <ThemedToaster />
+          </CounterSessionProvider>
         </QuoteCartProvider>
       </ThemeProvider>
     </QueryClientProvider>
