7282cf78d21d9684dcc0abac1f47514098acfe19
Matheus Francisco <matheusf.ls@hotmail.com>
2026-09-21 23:48:05 -0300

    fix(sessão): remover cache global do React Query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,25 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './experience-polish.css'
 import App from './App.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider, ThemedToaster } from './components/ThemeProvider'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      refetchOnWindowFocus: false,
-    },
-  },
-})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="system" storageKey="cognivault-theme">
-        <App />
-        <ThemedToaster />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="system" storageKey="cognivault-theme">
+      <App />
+      <ThemedToaster />
+    </ThemeProvider>
   </StrictMode>,
 )
